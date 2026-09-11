d64a6515eff6a516c888c9f30bb957fd50941d64
Esylum <brigade.205@gmail.com>
2026-09-11 16:39:43 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,29 +1,29 @@
 
 const config = {
 
-    token: 'Bot_Token',
+    token: 'MTU0Nzg3MDc2OTMyMzk2NjQ4NA.GRutPA.GtXsmCwoxeW7OY23Isk7I6s5jkSPnISPy89U08',
 
-    mongo: "mongo_url",
+    mongo: "mongodb+srv://<brigade205_db_user>:UnNS26vyPq1cYuVX@cluster0.l3kngdr.mongodb.net/?appName=Cluster0",
 
-    guildid: "guild_id",
+    guildid: "1547422235860537476",
 
     prefix: ['+','-'],
 
-    vouch__channel_id: '' || undefined,
+    vouch__channel_id: '1547422648500486197' || undefined,
 
-    approveVouch_channel_id: '' || undefined,
+    approveVouch_channel_id: '1547422648500486197' || undefined,
     
-    DeniedVouch_channel_id: '' || undefined,
+    DeniedVouch_channel_id: '1547422648500486197' || undefined,
     
-    verificationVouch_channel_id: '' || undefined,
+    verificationVouch_channel_id: '1547422648500486197' || undefined,
 
-    VouchStaff_Roleid: '' || undefined,
+    VouchStaff_Roleid: '1547422856449884250' || undefined,
 
-    admin_roleId: '' || undefined,  // Mark_Permission_Role_id  Changed To admin-Roleid 
+    admin_roleId: '1547422856449884250' || undefined,  // Mark_Permission_Role_id  Changed To admin-Roleid 
 
-    ScammerMark_Channel_id: '' || undefined,
+    ScammerMark_Channel_id: '1547422856449884250' || undefined,
 
-    DwcMark_Channel_id: '' || undefined
+    DwcMark_Channel_id: '1547422856449884250' || undefined
 
     
 
